efaf178791f661daa47996bc603e93b5790816af
Mohamed <mohamedemadeldinnn2@gmail.com>
2026-03-24 12:05:53 +0200

    feat: add api responses trait

---
+++ app/Utilities/APIResponses.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+trait APIResponses
+{
+    protected function success(array|object $data, string $message, int $code = Response::HTTP_OK): JsonResponse
+    {
+        return response()->json([
+            'status' => 'Success',
+            'message' => $message ?? null,
+            'data' => $data,
+        ], $code);
+    }
+
+    protected function fail(string $message, int $code = Response::HTTP_BAD_REQUEST): JsonResponse
+    {
+        return response()->json([
+            'status' => 'Failed',
+            'message' => $message ?? null,
+        ], $code);
+    }
+
+    protected function noContent(): Response
+    {
+        return response()->noContent();
+    }
+}
