fe0a74cd632ef8c7901ba6529d6663f804a3e6d2
Mohamed <mohamedemadeldinnn2@gmail.com>
2026-03-24 12:34:08 +0200

    feat: add api versioning

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: 'api',
+        then: function (): void {
+            Route::prefix('api/v1')
+                ->middleware('api')
+                ->group(function (): void {
+                    require __DIR__.'/../routes/api.php';
+                });
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
